6ef5313bf611ae25b0e0c693039f0948e61ee3c1
MidnightChaos <aidan.yung@gmail.com>
2026-03-01 17:25:43 -0700

    initial commit

---
+++ src/GCaLink/GCaLink/Platform/Win32Interlop.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCaLink.Platform
+{
+    class Win32Interlop
+    {
+        private const int GWL_EXSTYLE = -20;
+
+        private const int WS_EX_TRANSPARENT = 0x00000020;
+        private const int WS_EX_LAYERED = 0x00080000;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowLongPtr(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPos(
+            IntPtr hWnd,
+            IntPtr hWndInsertAfter,
+            int X,
+            int Y,
+            int cx,
+            int cy,
+            uint uFlags);
+
+        private static readonly IntPtr HWND_TOP = new IntPtr(0);
+        private static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+
+        private const uint SWP_NOMOVE = 0x0002;
+        private const uint SWP_NOSIZE = 0x0001;
+        private const uint SWP_SHOWWINDOW = 0x0040;
+
+        public static void EnableClickThrough(IntPtr hwnd)
+        {
+            var styles = GetWindowLongPtr(hwnd, GWL_EXSTYLE).ToInt64();
+            styles |= WS_EX_TRANSPARENT | WS_EX_LAYERED;
+            SetWindowLongPtr(hwnd, GWL_EXSTYLE, new IntPtr(styles));
+        }
+
+        public static void DisableClickThrough(IntPtr hwnd)
+        {
+            var styles = GetWindowLongPtr(hwnd, GWL_EXSTYLE).ToInt64();
+            styles &= ~WS_EX_TRANSPARENT;
+            SetWindowLongPtr(hwnd, GWL_EXSTYLE, new IntPtr(styles));
+        }
+
+        public static void BringToFront(IntPtr hwnd)
+        {
+            SetWindowPos(hwnd, HWND_TOP, 0, 0, 0, 0,
+                SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+        }
+
+        public static void RemoveTopMost(IntPtr hwnd)
+        {
+            SetWindowPos(hwnd, HWND_NOTOPMOST, 0, 0, 0, 0,
+                SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+        }
+
+    }
+}

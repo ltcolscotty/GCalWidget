04f4e37334fd69e386acb830c4ee3635661fa792
MidnightChaos <Aidan.yung@gmail.com>
2026-03-04 14:16:15 -0700

    initial branch commit

---
+++ src/GCaLink/GCaLink/Utility/GCal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// probably useful https://stackoverflow.com/questions/24057939/login-using-google-oauth-2-0-with-c-sharp
+
+
+namespace GCaLink.Components
+{
+    class GCal
+    {
+    }
+}

c107d656c000d34a34f468ad9f29288d81f3499c
MidnightChaos <aidan.yung@gmail.com>
2026-03-21 23:56:53 -0700

    initial google calendar commit

---
+++ src/GCaLink/GCaLink/Services/googleCal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.Threading;
+
+namespace GCaLink.Services
+{
+    internal class googleCal
+    {
+    }
+}

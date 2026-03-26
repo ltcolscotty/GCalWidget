059e56fd788e425c3bae5b7dfe22737f685b04c6
MidnightChaos <Aidan.yung@gmail.com>
2026-03-25 17:29:04 -0700

    Calendar options object

---
+++ src/GCaLink/GCaLink/Models/GoogleCalOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCaLink.Models
+{
+    public sealed class GoogleCalOptions
+    {
+        public string ClientId { get; init; } = "";
+        public string ClientSecret { get; init; } = "";
+        public string TokenPath { get; init; } = "";
+        public string DefaultColor { get; init; } = "#ff00ff";
+    }
+}

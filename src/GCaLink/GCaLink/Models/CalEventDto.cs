220eaaeda86bd0d7362000fcb6f79bf6ee157e45
MidnightChaos <Aidan.yung@gmail.com>
2026-03-25 17:27:17 -0700

    Event format object

---
+++ src/GCaLink/GCaLink/Models/CalEventDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCaLink.Models
+{
+    public sealed class CalEventDto
+    {
+        public string Title { get; init; } = "";
+        public string Datetime { get; init; } = "";
+        public string Link { get; init; } = "";
+        public bool CustomConfig { get; init; } = false;
+        public string Image { get; init; } = "";
+        public string Color { get; init; } = "";
+        public string Source { get; init; } = "";
+    }
+}

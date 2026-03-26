98e2620da50a49eb9dc10ed6534649827ac13a08
MidnightChaos <Aidan.yung@gmail.com>
2026-03-26 16:11:52 -0700

    initial settings config

---
+++ src/GCaLink/GCaLink/Models/ConfigOptions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GCaLink.Models
+{
+    public class ConfigOptions
+    {
+        public string CanvasICSLink { get; set; } = "";
+        public int BackgroundTransparency { get; set; } = 0;
+        public string BackgroundColor { get; set; } = "#ff00ff";
+        public string BackgroundImage { get; set; } = "";
+        /* color
+         * image
+         */
+        public string BackgroundSetting { get; set; } = "color";
+        public Boolean UseAcrylic { get; set; } = true;
+        public Font UsedFont = SystemFonts.DefaultFont;
+    }
+}

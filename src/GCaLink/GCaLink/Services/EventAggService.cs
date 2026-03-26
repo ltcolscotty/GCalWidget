1bdc94e18c339e41582833af60781a023b703b15
MidnightChaos <Aidan.yung@gmail.com>
2026-03-25 23:18:59 -0700

    initial commit for event aggregation

---
+++ src/GCaLink/GCaLink/Services/EventAggService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GCaLink.Models
+
+namespace GCaLink.Services
+{
+    internal class EventAggService
+    {
+
+        public async Task<Dictionary<string, CalEventDto>>
+    }
+}

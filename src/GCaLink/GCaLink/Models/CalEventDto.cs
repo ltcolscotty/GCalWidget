27fe1533f7e3090faf7626eebd85d5270d04d1b9
Justin Naicker <JustinNaickerWork@gmail.com>
2026-03-27 15:31:33 -0700

    Fixed initalization

--- src/GCaLink/GCaLink/Models/CalEventDto.cs
+++ src/GCaLink/GCaLink/Models/CalEventDto.cs
@@ -8,7 +8,7 @@ namespace GCaLink.Models
         [Key(0)] public string Title { get; set; } = "";
         [Key(1)] public string Datetime { get; set; } = "";
         [Key(2)] public string Link { get; set; } = "";
-        [Key(3)] public bool CustomConfig { get; set; }
+        [Key(3)] public bool CustomConfig { get; set; } = false;
         [Key(4)] public string Image { get; set; } = "";
         [Key(5)] public string Color { get; set; } = "";
         [Key(6)] public string Source { get; set; } = "";

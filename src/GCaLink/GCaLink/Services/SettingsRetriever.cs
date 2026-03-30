7c332bbb506bfdf9c3d2e0fcccfec9905cd9304a
MidnightChaos <Aidan.yung@gmail.com>
2026-03-30 00:11:21 -0700

    starting work

--- src/GCaLink/GCaLink/Services/SettingsRetriever.cs
+++ src/GCaLink/GCaLink/Services/SettingsRetriever.cs
@@ -8,9 +8,27 @@ using System.IO;
 using System.Text;
 using System.Text.Json;
 
+using GCaLink.Models;
+
 namespace GCaLink.Services
 {
     internal class SettingsRetriever
     {
+        public SettingsRetriever() {
+            string appDataLocalPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string appDataLocalFolder = Path.Combine(appDataLocalPath, "GCWidget");
+            string settingsFile = Path.Combine(appDataLocalFolder, "GCWConfig.json");
+
+            if (!Directory.Exists(appDataLocalFolder))
+            {
+                Directory.CreateDirectory(appDataLocalFolder);
+            }
+
+            if (!File.Exists(settingsFile))
+            {
+                ConfigOptions newOptions = new ConfigOptions();
+                string jsonString = JsonSerializer.Serialize(newOptions);
+            }
+        }
     }
 }

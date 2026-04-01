5f5019ed292db2ffc364c164df32de9ec7d536c4
MidnightChaos <aidan.yung@gmail.com>
2026-04-01 13:01:29 -0700

    initial commit for CanvasService

---
+++ src/GCaLink/GCaLink/Services/CanvasService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GCaLink.Models;
+
+namespace GCaLink.Services
+{
+    internal class CanvasService
+    {
+        public CanvasService() { 
+            IcsDownloader downloader = new IcsDownloader();
+        }
+
+        public async Task FetchUpcomingEventsAsync(string sourceLink)
+        {
+
+        }
+    }
+}

32fe911e1971af203d1eddef80ee863e8862d960
MidnightChaos <Aidan.yung@gmail.com>
2026-08-25 16:00:19 -0700

    id hashing for local management

---
+++ src/GCaLink/GCaLink/Services/IDHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace GCaLink.Services
+{
+    public interface IDHelper
+    {
+        public readonly record struct EventID(string Value);
+
+        public static EventID GetEventID
+            (
+                string eId="", 
+                string source="",
+                string owner=""
+            )
+        {
+            byte[] input = Encoding.UTF8.GetBytes(eId + owner + source);
+            using var sha1 = SHA1.Create();
+            byte[] hash = sha1.ComputeHash(input);
+            return new EventID(Convert.ToHexString(hash).ToLowerInvariant());
+        }
+    }
+}

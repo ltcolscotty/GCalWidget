ec90b4e80dd28419ef6a0e550d513a0503cf4275
Aidan Yung <99507808+ltcolscotty@users.noreply.github.com>
2026-09-04 20:00:29 +0000

    Attempt 2 to address build issues

--- src/GCaLink/GCaLink/ViewWindows/SettingsView/SettingsWindow.xaml.cs
+++ src/GCaLink/GCaLink/ViewWindows/SettingsView/SettingsWindow.xaml.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
 
-namespace GCaLink.ViewWindows.SettingsWindow;
+namespace GCaLink.ViewWindows.SettingsView;
 
-class SettingsWindow
+public sealed partial class SettingsWindow : Window
 {
+	public SettingsWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void myButton_Click(object sender, RoutedEventArgs e)
+	{
+	}
 }
--- src/GCaLink/GCaLink/ViewWindows/WidgetView/WidgetWindow.xaml.cs
+++ src/GCaLink/GCaLink/ViewWindows/WidgetView/WidgetWindow.xaml.cs
@@ -26,7 +26,7 @@ using System.Diagnostics.Contracts;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace GCaLink.ViewWindows.WidgetWindow
+namespace GCaLink.ViewWindows.WidgetView
 {
     // <summary>
     // An empty window that can be used on its own or navigated to within a Frame.
